91d97fd8f8fd273c468ddc837af661f60ad331da
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-01 05:59:03 +0000

    [応用課題２]User(編集画面表示・編集機能実装)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "show"
     end
