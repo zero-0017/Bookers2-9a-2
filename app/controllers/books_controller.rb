d7d97825f9812a5d11337a5a2c50cd36d6ccd460
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-01 07:47:58 +0000

    [応用課題２]Book(投稿削除機能実装)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,9 +34,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
