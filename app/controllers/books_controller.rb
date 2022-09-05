759cb155acf986c5dd02add4340498e78c844920
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-05 02:56:03 +0000

    [応用課題7a]過去１週間のいいねの多い順に投稿を表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,13 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
     @user = current_user
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+     }
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :favorited_users, through: :favorites, source: :user
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
